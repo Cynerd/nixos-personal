902f91984818f3ed0ab8b50f95e23aa9b2ab64a0
Karel Kočí <cynerd@email.cz>
2022-06-29 16:00:56 +0200

    pkgs/ferdium: update to version 6.0.0-nightly.80

--- pkgs/ferdium/default.nix
+++ pkgs/ferdium/default.nix
@@ -17,10 +17,10 @@ in
 mkFranzDerivation' rec {
   pname = "ferdium";
   name = "Ferdium";
-  version = "6.0.0-nightly.74";
+  version = "6.0.0-nightly.80";
   src = fetchurl {
     url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/ferdium_${version}_amd64.deb";
-    sha256 = "11viyqw41rzqs7bn7lagsjk7knb9xl5w6hpp5lbw7x1wqqyw9hv3";
+    sha256 = "10gl3750nf2p6nqpm60ffbilrcr70wn2whwk94cyszq2d3nwg4cc";
   };
   extraBuildInputs = [ xorg.libxshmfence ];
   meta = with lib; {
