39cf2de3cc5840c3e7fb448f55dd0b566f2a47d3
Karel Kočí <cynerd@email.cz>
2022-08-18 09:59:03 +0200

    devShells/qt: get shv build

--- devShells/qt.nix
+++ devShells/qt.nix
@@ -3,9 +3,21 @@ let
   pkgs = nixpkgs.legacyPackages.${system};
 
 in pkgs.mkShell {
-  packages = (with pkgs; [
-    qt5.full
+  packages = (with pkgs; with libsForQt5; [
     cmake
+    qt5.full
+    doctest
+
+    (qcoro.overrideAttrs (oldAttrs: {
+      version =  "0.6.1";
+      src = fetchFromGitHub {
+        owner = "danvratil";
+        repo = "qcoro";
+        rev = "261663560f59a162c0c82158a6cde41089668871";
+        sha256 = "OAYJpoW3b0boSYBfuzLrFvlYSmP3SON8O6HsDQoi+I8=";
+      };
+      buildInputs = oldAttrs.buildInputs ++ [qt5.full];
+    }))
   ]);
   inputsFrom = with pkgs; [ default ];
   meta.platforms = nixpkgs.lib.platforms.linux;
