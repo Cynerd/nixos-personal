fbc988088c25ab6a81e786a9554cb03e8ead4958
Karel Kočí <cynerd@email.cz>
2022-11-15 15:07:34 +0100

    pkgs/dev: allow running without argument

--- pkgs/dev/dev.sh
+++ pkgs/dev/dev.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -eu
 
-target="$1"
+target="${1:-}"
 shift
 
+declare -a nixargs
 known_shells="$(tr ':' '\n' <<<"${DEV_SHELLS:-}")"
 while IFS='=' read name drv res; do
 	if [ "$target" == "$name" ]; then
 		target="$drv"
+		# Note: we do not need substituters as this should be build
+		nixargs+=("--no-substitute")
 		break
 	fi
 done <<<"$known_shells"
 
-exec nix develop "$target" -c zsh "$@"
+exec nix "${nixargs[@]}" develop "$target" -c zsh "$@"
