d3575b392bb3091f0a542d08bd8074d0a7d0b64d
Karel Kočí <cynerd@email.cz>
2023-03-10 09:32:45 +0100

    devShells/qt: update to build brclab

--- devShells/qt.nix
+++ devShells/qt.nix
@@ -9,26 +9,13 @@ in
   pkgs.mkShell {
     packages = with pkgs;
     with libsForQt5; [
-      #full
       qtbase
+      qttranslations
       qtserialport
       qtwebsockets
       doctest
       qtcharts
       qtwayland
-
-      (stdenv.mkDerivation {
-        pname = "qcoro";
-        version = "0.6.1";
-        src = fetchFromGitHub {
-          owner = "danvratil";
-          repo = "qcoro";
-          rev = "261663560f59a162c0c82158a6cde41089668871";
-          sha256 = "OAYJpoW3b0boSYBfuzLrFvlYSmP3SON8O6HsDQoi+I8=";
-        };
-        buildInputs = [qtbase qtwebsockets];
-        nativeBuildInputs = [wrapQtAppsHook cmake];
-      })
     ];
     inputsFrom = with pkgs; [default c];
     meta.platforms = ["x86_64-linux"];
