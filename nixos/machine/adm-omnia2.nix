a2f35401414d47c1a929b5b75d5e40705954b00d
Karel Kočí <cynerd@email.cz>
2024-03-15 10:56:26 +0100

    adm-omnia2: update and redeploy

--- nixos/machine/adm-omnia2.nix
+++ nixos/machine/adm-omnia2.nix
@@ -1,4 +1,9 @@
 {config, ...}: {
+  deploy = {
+    enable = true;
+    ssh.host = "omnia2.adm";
+  };
+
   cynerd = {
     switch = {
       enable = true;
@@ -7,10 +12,22 @@
     };
     wifiAP.adm = {
       enable = true;
-      ar9287.interface = "wlp2s0";
-      qca988x.interface = "wlp1s0";
+      ar9287 = {
+        interface = "wlp1s0";
+        bssids = ["12:f0:21:23:2b:00" "12:f0:21:23:2b:01" "12:f0:21:23:2b:02"];
+        channel = 11;
+      };
+      qca988x = {
+        interface = "wlp2s0";
+        bssids = ["12:f0:21:23:2b:03" "12:f0:21:23:2b:04" "12:f0:21:23:2b:05"];
+        channel = 36;
+      };
     };
   };
+
+  services.journald.extraConfig = ''
+    SystemMaxUse=8G
+  '';
 
   services.btrfs.autoScrub = {
     enable = true;
@@ -23,7 +40,7 @@
   };
   systemd.network.networks = {
     "lan-brlan" = {
-      matchConfig.Name = "lan* eth0";
+      matchConfig.Name = "lan* end2";
       networkConfig.Bridge = "brlan";
       bridgeVLANs = [
         {
