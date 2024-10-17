639fd2cc86123f7a34300a645cc1bc5ebf83ad1a
Karel Kočí <cynerd@email.cz>
2024-10-17 15:38:39 +0200

    nixos/generic: remove obsolete repl-flake nix option

--- nixos/modules/generic.nix
+++ nixos/modules/generic.nix
@@ -9,7 +9,7 @@ in {
     system.stateVersion = "24.05";
 
     nix = {
-      extraOptions = "experimental-features = nix-command flakes repl-flake";
+      extraOptions = "experimental-features = nix-command flakes";
       settings = {
         auto-optimise-store = true;
         substituters = [
